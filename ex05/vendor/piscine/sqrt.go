a9f608a0331d74126ac926a88c5667eae02d8a91
Nao Sakanoue <nsakanou@c1r5s8.42tokyo.jp>
2024-06-27 18:22:51 +0900

    Sqrt_overflow_countermeasure

--- ex05/vendor/piscine/sqrt.go
+++ ex05/vendor/piscine/sqrt.go
@@ -6,7 +6,7 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 16:31:20 by nsakanou          #+#    #+#             */
-/*   Updated: 2024/06/27 16:37:02 by nsakanou         ###   ########.fr       */
+/*   Updated: 2024/06/27 18:21:48 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,25 @@ func Sqrt(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-	for i := 0; i * i <= nb; i++ {
-		if i * i == nb {
+	for i := uint64(0); i * i <= uint64(nb); i++ {
+		if i * i < 0 {
+			return -42
+		}
+		if i * i == uint64(nb) {
 			return i
 		}
 	}
 	return 0
 }
+
+// func Sqrt(nb int) int {
+// 	if nb < 0 {
+// 		return 0
+// 	}
+// 	for i := 0; i * i <= nb; i++ {
+// 		if i * i == nb {
+// 			return i
+// 		}
+// 	}
+// 	return 0
+// }
